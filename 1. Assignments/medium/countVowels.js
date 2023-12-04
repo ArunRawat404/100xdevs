c81595bcf704107712ee6f60d6a2da310ec59471
arunrawat404 <arunrawatar404@gmail.com>
2023-12-04 22:23:38 +0530

    Solved medium problems of assignment

--- 1. Assignments/medium/countVowels.js
+++ 1. Assignments/medium/countVowels.js
@@ -6,7 +6,17 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  str = str.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = countVowels;
--- 1. Assignments/medium/palindrome.js
+++ 1. Assignments/medium/palindrome.js
@@ -1,9 +1,33 @@
 /*
   Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
-  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrome as 'N' and 'n' are considered case-insensitive.
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  // remove all spaces
+  str = str.replace(/ /g, '');
+
+  let start = 0;
+  end = str.length - 1;
+
+  specialChar = ["'", "!", ",", ".", "?"];
+
+  while (start < end) {
+    if (specialChar.includes(str[start])) {
+      start++;
+    }
+    if (specialChar.includes(str[end])) {
+      end--;
+    }
+
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
   return true;
 }
 
