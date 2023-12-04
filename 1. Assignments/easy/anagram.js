a83ed52416151eb3b2be55547a41c1d6eeb1473e
arunrawat404 <arunrawatar404@gmail.com>
2023-12-04 20:15:12 +0530

    Solved Easy problems of assignment

--- 1. Assignments/easy/anagram.js
+++ 1. Assignments/easy/anagram.js
@@ -5,7 +5,50 @@
 */
 
 function isAnagram(str1, str2) {
+  // to check if both strings have same length if not then return false
 
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+  // convert both strings to lowercase
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  // create an object to store characters and their count
+  const charCount = {};
+
+  // traverse through first string
+  for (let i = 0; i < str1.length; i++) {
+    if (!charCount[str1[i]]) {
+      charCount[str1[i]] = 1;
+    } else {
+      charCount[str1[i]]++;
+    }
+  }
+
+  // traverse through first string
+  for (let i = 0; i < str2.length; i++) {
+    // if character is not in object then return false
+    if (!charCount[str2[i]]) {
+      return false;
+    } else {
+      // if character is in object then decrement its count
+      charCount[str2[i]]--;
+    }
+  }
+
+  // traverse through object
+  for (let key in charCount) {
+    // if any character count is not 0 then return false
+    if (charCount[key] !== 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
--- 1. Assignments/easy/expenditure-analysis.js
+++ 1. Assignments/easy/expenditure-analysis.js
@@ -6,7 +6,25 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotals = {};
+
+  transactions.forEach(transaction => {
+    // object destructuring
+    const { category, price } = transaction;
+
+    if (categoryTotals[category] == undefined) {
+      categoryTotals[category] = price;
+    } else {
+      categoryTotals[category] += price;
+    }
+  });
+
+  const result = Object.keys(categoryTotals).map((category) => ({
+    "category": category,
+    "totalSpent": categoryTotals[category]
+  }));
+
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
