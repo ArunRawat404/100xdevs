e814eefdb21d8c66a7310bdadd7d371e3d4fffda
arunrawat404 <arunrawatar404@gmail.com>
2023-12-16 00:45:52 +0530

    Solved hard problems of async js assignment

--- 2. Async JS Assignments/hard (promises)/3-promise-all.js
+++ 2. Async JS Assignments/hard (promises)/3-promise-all.js
@@ -5,19 +5,39 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function wait2(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function wait3(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startTime = Date.now();
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)])
+        .then(function (results) {
+            const endTime = Date.now();
+            const timeTaken = (endTime - startTime);
+            console.log("Time taken for resolving all promises" + timeTaken + " seconds");
+            console.log("Results:", results);
+            return timeTaken
+        })
 }
 
 module.exports = calculateTime;
--- 2. Async JS Assignments/hard (promises)/4-promise-chain.js
+++ 2. Async JS Assignments/hard (promises)/4-promise-chain.js
@@ -6,19 +6,35 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function wait2(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function wait3(t) {
-
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            resolve();
+        }, t * 1000)
+    });
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const startTime = Date.now();
+    return wait1(t1)
+        .then(() => { return wait2(t2) })
+        .then(() => { return wait3(t3) })
+        .then(() => { return Date.now() - startTime })
 }
 
 module.exports = calculateTime;
