0282ba26ffc7f4b026deb2625117843da3b71e56
arunrawat404 <arunrawatar404@gmail.com>
2023-12-19 16:37:53 +0530

    Added validation using zod

--- 3. Middlewares, Zod/index.js
+++ 3. Middlewares, Zod/index.js
@@ -1,10 +1,32 @@
 const express = require("express");
+const zod = require("zod");
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3000;
+
+// array of number schema
+const schema = zod.array(zod.number());
+
+/* 
+{
+    email: string, should look like email
+    password: atleast 8 characters long
+    country: "IN" or "US"
+}
+
+schema for above object in zod
+
+const schemaObj = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8), // Ensures password is at least 8 characters long
+    country: zod.literal("IN", "US") // Uses .literal() to specify multiple literals
+});
+*/
+
+// middlewares
 
 function userMiddleware(req, res, next) {
     const username = req.headers.username;
@@ -34,9 +56,16 @@ app.get("/health-checkup", userMiddleware, kidneyMiddleware, function (req, res)
 
 app.post("/health-checkup", function (req, res) {
     const kidneys = req.body.kidneys;
-    const kidneyLength = kidneys.length;
-
-    res.send("You have " + kidneyLength + " kidneys");
+    // validation
+    const response = schema.safeParse(kidneys);
+    if (!response.success) {
+        res.status(411).json({
+            mss: "Input is invalid"
+        })
+    } else {
+        const kidneyLength = kidneys.length;
+        res.send("You have " + kidneyLength + " kidneys");
+    }
 });
 
 // global catches
